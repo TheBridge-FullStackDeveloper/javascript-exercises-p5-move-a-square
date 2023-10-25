44751d36837cf6c16de73483cac76fad7ff53b27
Cristina <c.gomezlop95@gmail.com>
2023-10-25 20:45:50 +0200

    step 1 and 2 done

---
+++ sketch.js
@@ -0,0 +1,11 @@
+let x = 200;
+let y = 200;
+let width = 50;
+let height = 50;
+
+function setup() {
+  createCanvas(400, 400);
+  background(200); //Color gris para el fondo
+  fill(255, 0, 0); //Dar color rojo al círculo
+  rect(x, y, width, height); //Creación de mi rectángulo
+}
