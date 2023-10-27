fc0731ff4ed0c6cc46f21774e16a08e623586d04
Cristina <c.gomezlop95@gmail.com>
2023-10-27 12:17:23 +0200

    step 3 and 4 done

--- sketch.js
+++ sketch.js
@@ -9,3 +9,24 @@ function setup() {
   fill(255, 0, 0); //Dar color rojo al círculo
   rect(x, y, width, height); //Creación de mi rectángulo
 }
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    //disminuye la posición x
+    x -= 10;
+  } else if (keyCode === RIGHT_ARROW) {
+    //aumenta la posición x
+    x += 10;
+  } else if (keyCode === UP_ARROW) {
+    //disminuye la posición y
+    y -= 10;
+  } else if (keyCode === DOWN_ARROW) {
+    //aumenta la posición y
+    y += 10;
+  }
+}
+
+function draw() {
+  background(200); //This is necessary because it erases the previous frame's content, preventing it from accumulating on the canvas.
+  rect(x, y, width, height); // Redraw the rectangle at the updated position
+}
